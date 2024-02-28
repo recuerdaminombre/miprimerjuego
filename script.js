bd5ff327f9c0f7ca3c298ae23bfed2198895bc37
recuerdaminombre <161613065+recuerdaminombre@users.noreply.github.com>
2024-02-28 10:11:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let nclicks = 0;
+const imagen = document.querySelector(".imagen");
+const intruso = document.querySelector("#intruso");
+const clicks = document.querySelector(".clicks");
+const alerta = document.querySelector(".alerta")
+const alertaMensaje = document.querySelector(".alerta-mensaje")
+
+intruso.style.backgroundColor = "rgba(0,0,0,0)";
+
+intruso.addEventListener("click", function() {
+    imagen.style.border = "10px solid green";
+    imagen.style.borderRadius = "5%";
+    imagen.style.backgroundColor = "green";
+    alerta.style.display = "flex";
+    
+    if (nclicks == 1) {
+        alertaMensaje.textContent = "Eres un crack, encontraste al impostor a la primera";
+    } else if (nclicks < 11) {
+        alertaMensaje.textContent = `Buen trabajo, encontraste al impostor después de ${nclicks}`;
+    } else {
+        alertaMensaje.textContent = "Eres un ciego, pero igual lo lograste";
+    }
+})
+
+imagen.addEventListener("click", function() {
+    nclicks = nclicks + 1;
+    if (nclicks == 1) {
+        clicks.style.color = "green";
+    } else if (nclicks < 11) {
+        clicks.style.color = "yellow";
+    } else {
+        clicks.style.color = "red";
+    }
+    clicks.textContent = `Clicks: ${nclicks}`;
+})
+
+function recargar () {
+    location.reload()
+}
